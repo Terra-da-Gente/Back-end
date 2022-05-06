2ad30c17408ef96921102a9722d961d4aa0e069a
gustavo <gustavohms1998@gmail.com>
2022-05-06 10:36:06 -0300

    Class Controller Atualizada

---
+++ src/main/java/com/generation/terradagente/controller/CategoriaController.java
@@ -0,0 +1,29 @@
+package com.generation.terradagente.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.terradagente.model.Categoria;
+import com.generation.terradagente.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List <Categoria>> findAll(){
+		
+		
+	}
+	
+}
