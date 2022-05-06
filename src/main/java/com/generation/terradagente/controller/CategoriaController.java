3c9d8aa3cd97e4cdd75bad461db0d1dac1645ba0
LeZuniga <leh.zuniga@gmail.com>
2022-05-06 10:52:56 -0300

    classe controladora atualizada 2

--- src/main/java/com/generation/terradagente/controller/CategoriaController.java
+++ src/main/java/com/generation/terradagente/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,21 @@ public class CategoriaController {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
-	public ResponseEntity<List <Categoria>> findAll(){
-		
-		
+	public ResponseEntity<List <Categoria>> getAll(){
+		return ResponseEntity.ok(categoriaRepository.findAll());
 	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable Long id){
+		return categoriaRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Categoria>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(categoriaRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	
+	
 	
 }
