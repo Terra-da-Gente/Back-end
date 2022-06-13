fb273497ff08b75f0363e500bb964aac4aa9f64e
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-06-13 19:35:17 -0300

    produtos update

--- src/main/java/com/generation/terradagente/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/terradagente/security/BasicSecurityConfig.java
@@ -39,6 +39,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
+			.antMatchers("/produto").permitAll()
+			.antMatchers("/categoria").permitAll()
 			.antMatchers("/usuarios/logar").permitAll()
 			.antMatchers("/usuarios/cadastrar").permitAll()
 			.antMatchers(HttpMethod.OPTIONS).permitAll()
